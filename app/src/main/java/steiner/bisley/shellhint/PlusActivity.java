a3967bbfde025985c24083da4462169bc065a506
SarifIndustries <leveldefault@yahoo.com>
2018-11-10 03:25:06 +0300

    Further Upgrade
    New picture, FAB button, Snackbar, Material Design and more.

--- app/src/main/java/steiner/bisley/shellhint/PlusActivity.java
+++ app/src/main/java/steiner/bisley/shellhint/PlusActivity.java
@@ -1,10 +1,12 @@
 package steiner.bisley.shellhint;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class PlusActivity extends AppCompatActivity {
 
@@ -20,5 +22,19 @@ public class PlusActivity extends AppCompatActivity {
         // Deploy Home button on toolbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickFAB(View view) {
+        CharSequence text = "Thank you for contribution!";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snack = Snackbar.make(findViewById(R.id.PlusCoordinatorID), text, duration);
+        snack.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast tos = Toast.makeText(PlusActivity.this, "No problem.", Toast.LENGTH_SHORT);
+                tos.show();
+            }
+        });
+        snack.show();
     }
 }
